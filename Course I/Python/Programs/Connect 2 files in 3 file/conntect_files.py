7ccc1b5aba2165671f8b4ff7d744e7d9af4f7435
FlymeDllVa <flymedllva@gmail.com>
2018-11-26 20:32:26 +0300

    Added 3 versions of the problem solution

--- Course I/Python/Programs/Connect 2 files in 3 file/conntect_files.py
+++ Course I/Python/Programs/Connect 2 files in 3 file/conntect_files.py
@@ -1,4 +1,40 @@
-# Соеденить 2 файла в 3, при этом отсортировать их
+################################################################
+####### Соеденить 2 файла в 3, при этом отсортировать их #######
+################################################################
+
+''' 
+	########################
+	1 version, in with + defs 
+	########################
+'''
+# Str in Int
+def inInt(n): 
+	try:
+		return int(n)
+	except:
+		return None
+# Read line
+def select_readline(sected_file):
+	return sected_file.readline().rstrip()
+with open('file1.txt') as file1, open('file2.txt') as file2, open('file3.txt', 'w') as file3:
+	x = select_readline(file1)
+	y = select_readline(file2)
+	while (x or y) != '':
+		try:
+			if (inInt(y) == None) or (inInt(y) > inInt(x)):
+				print(x, file = file3)
+				x = select_readline(file1)
+			else:
+				print(y, file = file3)
+				y = select_readline(file2)
+		except Exception:
+			print(y, file = file3)
+			y = select_readline(file2)
+''' 
+	########################
+	2 version, list 
+	########################
+'''
 with open('file1.txt') as file1, open('file2.txt') as file2, open('file3.txt', 'w') as file3:
 	buff_file = []
 	while(True):
@@ -19,7 +55,43 @@ with open('file1.txt') as file1, open('file2.txt') as file2, open('file3.txt', '
 				pass
 			else:
 				buff_file.append(line2)
+	buff_file.sort()
 	for i in buff_file:
 		file3.write(str(i))
 		if i != buff_file[len(buff_file)-1]:
 			file3.write('\n')
+
+''' 
+	########################
+	3 version 
+	########################
+'''
+f1 = open(r"file1.txt", "r")
+f2 = open(r"file2.txt", "r")
+f3 = open(r"file3.txt", "w")
+s1 = (f1.readline()).strip('\n')
+s2 = (f2.readline()).strip('\n')
+x = s1
+y = s2
+def Inte(n):
+	try:
+		return int(n)
+	except:
+		return None 
+while (x or y) != '':
+	if Inte(x) == None:
+		print(y, file = f3)
+		y = (f2.readline()).strip('\n')
+	elif Inte(y) == None:
+		print(x, file = f3)
+		x = (f1.readline()).strip('\n')
+	elif (Inte(x) < Inte(y)):
+		print(x, file = f3)
+		x = (f1.readline()).strip('\n')
+		
+	else:
+		print(y, file = f3)
+		y = (f2.readline()).strip('\n')
+f1.close()
+f2.close()
+f3.close()
