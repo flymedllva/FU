c2e1c4ece5877528d5b200517faef1ee674678ad
Dmitry <flymedllva@gmail.com>
2018-12-01 14:19:41 +0300

    Update Получить все ссылки с сайта.py

--- Course I/Python/Programs/Получить все ссылки с сайта.py
+++ Course I/Python/Programs/Получить все ссылки с сайта.py
@@ -2,15 +2,19 @@
 def urlInSite(url):
 	from urllib.request import urlopen
 	from re import findall
-	with urlopen(url) as htmlFile:
-		data = htmlFile.read().decode('utf-8')
-		urls = findall(r'href=[\'"]?([^\'" >]+)', data)
-		allUrls = []
-		for i in urls:
-			url = findall(r'https://[\'"]?([^\'" >]+)', i)
-			if url:
-				allUrls.append('https://' + url[0])
-		return(allUrls)
+	try:
+		with urlopen(url) as htmlFile:
+			data = htmlFile.read().decode('utf-8')
+			urls = findall(r'href=[\'"]?([^\'" >]+)', data)
+			allUrls = []
+			for i in urls:
+				url = findall(r'https://[\'"]?([^\'" >]+)', i)
+				if url:
+					allUrls.append('https://' + url[0])
+			return(allUrls)
+	except Exception:
+		print('Не получить данные с сайта')
+		return []
 		
 url = "https://google.com"
 print(urlInSite(url))
