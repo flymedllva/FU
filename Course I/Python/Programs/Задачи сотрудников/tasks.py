a46fc024c3e8f7732b2168ed9758790bf67243f1
FlymeDllVa <flymedllva@gmail.com>
2018-12-06 09:44:07 +0300

    Update tasks.py

--- Course I/Python/Programs/Задачи сотрудников/tasks.py
+++ Course I/Python/Programs/Задачи сотрудников/tasks.py
@@ -2,10 +2,11 @@
 #2. Авторизация по логину/паролю или секретному ключу +
 #3. Два уровня доступа +
 #4. Хранение логинов/паролей +
+#5. Простейшее шифрование
 #	Администратор может:
 #		Регистрировать нового пользователя
-#		Назначать задачи пользователям
-#		Подтвердить выполнение
+#		Назначать задачи пользователям +
+#		Поставить задаче любого человека "выполнено" +
 #	Сотрудник может:
 #		Проверить свои задачи +
 #		Поставить задаче "выполнено" +
@@ -38,7 +39,9 @@ def checkPrivileges(x):
 # Check Complite Task
 def checkCompliteTask(x):
 	if x == 1:
-		return 'Выполнена'
+		return 'Выполнение подтвержено пользователем'
+	if x == 2:
+		return 'Выполнение подтвержено администратором'
 	return 'Не выполнена'
 
 ### Configuration
@@ -47,11 +50,10 @@ import pickle
 from multi_key_dict import multi_key_dict # Import multiple Dict keys
 
 FILE_NAME = 'base.pickle'
-userName = 'Flyme'
+userName = 'Ivan'
 userPassword = '12345'
 
 base = openFile(FILE_NAME) # Data ase
-print(base)
 if auth(base, userName, userPassword) != 0:
 		command = ''
 		currentUser = auth(base, userName, userPassword) # ['Login', 'Secret Key', 'Password', 'Privileges', 'Tasks'(1, 'Name Task')]
@@ -60,9 +62,10 @@ if auth(base, userName, userPassword) != 0:
 			### Admin
 			if checkPrivileges(currentUser[3]) == 'Администратор':
 				print('Доступные вам команды: ')
-				print('	0. Завершить программу\n	1. Мои задачи\n	2. Поставить выполено моей задаче\n	3. Новая задача\n	4. Подтвердить выполнение\n	5. Регистрировать нового пользователя')
+				print('	0. Завершить программу\n	1. Мои задачи\n	2. Поставить выполено моей задаче\n	3. Новая задача\n	4. Подтвердить выполнение задачи другого пользователя\n	5. Зарегистрировать нового пользователя')
 				print('\n{} выберите команду:'.format(currentUser[0]), end='')
 				command = input(' ')
+#				command = '4'
 				
 				if command == '1':
 					print('\nМои задачи:')
@@ -83,13 +86,30 @@ if auth(base, userName, userPassword) != 0:
 						print('   {}'.format(keys[0]))
 					try:
 						userNum = input('Введите имя пользователя, которому хотите дать новую задачу: ')
-						selectedUserTasks = base['Flyme'][4]
+						selectedUserTasks = base[userNum][4]
 						newTask = input('Введите новую задачу: ')
 						selectedUserTasks.append((0,newTask))
 					except Exception:
 						print('\nОшибка при обработке данных\nВыход...')
 						break
-					break
+				if command == '4':
+					print(base)
+					print('\nПользователи:')
+					for num,keys in enumerate(base.keys()):
+						print('   {}'.format(keys[0]))
+					try:
+						userNum = input('Введите имя пользователя, у которого требуется изменить готовность задачи: ')
+						selectedUserTasks = base[userNum][4]
+						print('\nЗадачи {}:'.format(userNum))
+						for num,i in enumerate(selectedUserTasks):
+							print('	(№{}) Задача {} {}'.format(num+1, checkCompliteTask(i[0]), i[1]))
+						print('\nВведите номер задачи, готовность которой, требуется изменить: ', end='')
+						task = input()
+						selectedUserTasks[int(task)-1] = (int(input('Выберите выполнена ли задача\n	1 – Выполнение подтвержено пользователем\n	2 – Выполнение подтвержено администратором\n	0 – не выполнена\n')), selectedUserTasks[int(task)-1][1])
+						print(selectedUserTasks[int(task)-1])
+					except Exception:
+						print('\nОшибка при обработке данных\nВыход...')
+						break
 			
 			### User
 			elif checkPrivileges(currentUser[3]) == 'Пользователь':
@@ -108,7 +128,10 @@ if auth(base, userName, userPassword) != 0:
 					print('\nВыберите номер вашей задачи, которой требуется изменить готовность: ', end='')
 					task = input()
 					ready = input('Выберите выполнена ли задача\n	1 – выполнена\n	0 – не выполнена\n')
-					tasksUser[int(task)-1] = (int(ready), tasksUser[int(task)-1][1])				
+					if ready == 1 or ready == 0:
+						tasksUser[int(task)-1] = (int(ready), tasksUser[int(task)-1][1])
+					else:
+						print('Недостаточно прав')			
 			### Other User
 			else:
 				print('Неизвестный пользователь')
