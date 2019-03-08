04ba66d1b95afa9c10d83d3912146cc98a7ebd95
Dmitry <flymedllva@gmail.com>
2019-03-09 01:56:35 +0300

    Create Задача 5 – Задача 5 – Наименьшее кратное.cpp

---
+++ Independent Activity/Project Euler/C++/Задача 5 – Задача 5 – Наименьшее кратное.cpp
@@ -0,0 +1,23 @@
+//  Задача 5
+//  Наименьшее кратное
+//
+//  2520 - самое маленькое число, которое делится без остатка на все числа от 1 до 10.
+//  Какое самое маленькое число делится нацело на все числа от 1 до 20?
+
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+    int number = 1;
+    int i = 1;
+    while (i <= 20)
+    {
+        if (number % i == 0) {
+            i++;
+        } else {
+            number++;
+            i = 1;
+        }
+    }
+    cout << number << endl;
+}
