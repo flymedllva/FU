d9301d5be5dec63b84fa8f75119b6c21e56cd734
Dmitry <flymedllva@gmail.com>
2019-04-05 22:15:25 +0300

    Create Convert given binary into hexadecimal.py

---
+++ Course I/Python/Programs/Convert given binary into hexadecimal.py
@@ -0,0 +1,6 @@
+# You must convert given binary into hexadecimal.
+# A single binary number T (0b followed by a string of 1's and 0's). The string length is N.
+# A hexadecimal representation of the binary number (0x followed by a string of chars in 0123456789abcdef)
+
+t = 0b1010
+print(hex(int(str(t), 2)))
